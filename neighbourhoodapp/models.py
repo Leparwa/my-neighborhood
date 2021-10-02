8f53c287b39cc6768476aad577cd871f55536747
leparwa <kevin.leparwa@student.moringaschool.com>
2021-10-02 18:39:56 +0300

    models

--- neighbourhoodapp/models.py
+++ neighbourhoodapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver 
+from django.db.models.signals import post_save 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=39)
+    occupants = models.IntegerField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=100)
+    business_email = models.EmailField(max_length=39)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=39)
+    occupants = models.IntegerField()
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.user.save()
